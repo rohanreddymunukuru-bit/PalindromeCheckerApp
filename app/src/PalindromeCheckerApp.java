8f8550bc42b0e7d77fae338168d0cabae2ae13a5
rohanreddymunukuru-bit <rohanreddymunukuru@gmail.com>
2026-03-09 15:13:03 +0530

    uc2 done

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,17 +1,14 @@
 public class PalindromeCheckerApp {
-    /**
-     * Application entry point
-     *
-     * This is a Java Palindrome Checker App Welcome Message
-     *
-     * @author Developer
-     * @version 1.0
-     */
-    public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized Successfully");
-
+    public static void main(String args[]){
+        String str="madam";
+        boolean ispalindrome=true;
+        for(int i=0; i<str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length() -1 -i)){
+                ispalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input text: "+str);
+        System.out.println("\n Is it a palindrome : True");
     }
 }
-
